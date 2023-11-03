2eff6990d601b4fb6f37d5821e18e89c5d25eff7
s.hapughin <s.hapughin@sportradar.com>
2023-11-03 21:27:39 +0100

    feat: add request logging 🔊

---
+++ src/main/java/windsurfersweatherapi/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,28 @@
+package windsurfersweatherapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+/**
+ * Configuration for requests logging.
+ */
+@Configuration
+public class RequestLoggingFilterConfig {
+  /**
+   * Filter properties setting.
+   *
+   * @return CommonsRequestLoggingFilter
+   */
+  @Bean
+  public CommonsRequestLoggingFilter logFilter() {
+    CommonsRequestLoggingFilter filter
+        = new CommonsRequestLoggingFilter();
+    filter.setIncludeQueryString(true);
+    filter.setIncludePayload(true);
+    filter.setMaxPayloadLength(10000);
+    filter.setIncludeHeaders(false);
+    filter.setAfterMessagePrefix("REQUEST DATA: ");
+    return filter;
+  }
+}
