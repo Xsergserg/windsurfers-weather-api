01268fef509cef15cc1127231b45b3f83c09a1fd
s.hapughin <s.hapughin@sportradar.com>
2023-11-03 16:42:53 +0100

    feat: init commit 🎉

---
+++ src/main/java/windsurfersweatherapi/WindsurfersWeatherApiApplication.java
@@ -0,0 +1,14 @@
+package windsurfersweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class.
+ */
+@SpringBootApplication
+public class WindsurfersWeatherApiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(WindsurfersWeatherApiApplication.class, args);
+  }
+}
---
+++ src/test/java/windsurfersweatherapi/WindsurfersWeatherApiApplicationTests.java
@@ -0,0 +1,11 @@
+package windsurfersweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindsurfersWeatherApiApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
