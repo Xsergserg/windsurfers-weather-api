a2ae70d5b141f7f45e6fc2e6a264594407e66891
s.hapughin <s.hapughin@sportradar.com>
2023-11-04 19:51:53 +0100

    test: add tests for WeatherBitForecastResponseMapper ✅

--- src/test/java/windsurfersweatherapi/config/MapperTest.java
+++ src/test/java/windsurfersweatherapi/config/MapperTest.java
@@ -1,19 +1,17 @@
 package windsurfersweatherapi.config;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static windsurfersweatherapi.config.MapperFactory.pissouriWeatherBitForecastResponseJson;
 import static windsurfersweatherapi.config.MapperFactory.readWeatherBitForecastResponseTree;
+import windsurfersweatherapi.mapper.WeatherBitForecastResponseMapper;
 import windsurfersweatherapi.model.Coordinates;
 import windsurfersweatherapi.model.LocationData;
 import windsurfersweatherapi.model.WeatherData;
@@ -21,15 +19,12 @@ import windsurfersweatherapi.model.WeatherForecast;
 
 @SpringBootTest
 class MapperTest {
-  @Autowired
-  private ObjectMapper objectMapper;
-
   @Test
   @DisplayName("JsonNode should map to WeatherData correctly")
-  public void whenJsonNodeMapsToWeatherForecast_ShouldBeExpected() throws JsonProcessingException {
+  public void jsonShouldBeMappedCorrectly() throws JsonProcessingException {
     JsonNode jsonNode = readWeatherBitForecastResponseTree(pissouriWeatherBitForecastResponseJson());
-    List<WeatherForecast> actual = objectMapper.convertValue(jsonNode, new TypeReference<>() {
-    });
+    WeatherBitForecastResponseMapper mapper = new WeatherBitForecastResponseMapper();
+    List<WeatherForecast> actual = mapper.convertToWeatherForecasts(jsonNode);
     List<WeatherForecast> expected = List.of(
         new WeatherForecast(
             LocalDate.of(2023, Month.NOVEMBER, 4),
