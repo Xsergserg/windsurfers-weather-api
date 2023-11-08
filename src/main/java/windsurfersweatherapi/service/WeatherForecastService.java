c7fb40ea65177deabd20dabdae40b11170910504
s.hapughin <s.hapughin@sportradar.com>
2023-11-08 22:54:33 +0100

    feat: add service functionality ✨

--- src/main/java/windsurfersweatherapi/service/WeatherForecastService.java
+++ src/main/java/windsurfersweatherapi/service/WeatherForecastService.java
@@ -1,11 +1,17 @@
 package windsurfersweatherapi.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import windsurfersweatherapi.client.WeatherBitForecastApiClient;
+import windsurfersweatherapi.enums.Location;
+import windsurfersweatherapi.mapper.WeatherBitForecastResponseMapper;
 import windsurfersweatherapi.model.WeatherForecast;
+import windsurfersweatherapi.service.validator.WeatherForecastServiceValidator;
 
 /**
  * Weather forecast service. Service accepts date as an argument, fetches forecast data with
@@ -17,19 +23,62 @@ import windsurfersweatherapi.model.WeatherForecast;
 @Service
 public class WeatherForecastService {
 
-  private final ObjectMapper mapper;
+  private final WeatherBitForecastResponseMapper mapper;
   private final WeatherBitForecastApiClient weatherBitForecastApiClient;
+  private final WeatherForecastServiceValidator validator;
 
+  /**
+   * Constructor.
+   *
+   * @param mapper                      mapper for converting jsonNode to WeatherForecasts
+   * @param weatherBitForecastApiClient API client
+   * @param validator                   validator
+   */
   @Autowired
-  public WeatherForecastService(
-      ObjectMapper mapper,
-      WeatherBitForecastApiClient weatherBitForecastApiClient
-  ) {
+  public WeatherForecastService(WeatherBitForecastResponseMapper mapper,
+      WeatherBitForecastApiClient weatherBitForecastApiClient,
+      WeatherForecastServiceValidator validator) {
     this.mapper = mapper;
     this.weatherBitForecastApiClient = weatherBitForecastApiClient;
+    this.validator = validator;
   }
 
-  WeatherForecast getBestWindsurfersForecast(Date date) {
-    return null;
+  /**
+   * Getting the best location for chosen date.
+   *
+   * @param date chosen date
+   * @return WeatherForecast
+   */
+  public WeatherForecast getBestWindsurfersForecast(LocalDate date) {
+    validator.validateDate(date);
+    return Arrays.stream(Location.values()).parallel()
+        .map(
+            node -> mapper.convertToWeatherForecasts(
+                weatherBitForecastApiClient.fetchForecasts(node)))
+        .filter(Objects::nonNull)
+        .flatMap(Collection::stream)
+        .peek(validator::validateForecast)
+        .filter(forecast -> {
+          Double averageWindSpeed = forecast.weatherData().averageWindSpeed();
+          Double averageTemp = forecast.weatherData().averageTemperature();
+          return forecast.date().equals(date)
+              && isSuitableWindSpeed(averageWindSpeed)
+              && isSuitableTemp(averageTemp);
+        })
+        .max(Comparator.comparing(this::calculateForecastRate))
+        .orElse(null);
+  }
+
+  private Double calculateForecastRate(WeatherForecast forecast) {
+    return forecast.weatherData().averageWindSpeed() * 3 + forecast.weatherData()
+        .averageTemperature();
+  }
+
+  private Boolean isSuitableWindSpeed(Double windSpeed) {
+    return windSpeed >= 5 && windSpeed <= 18;
+  }
+
+  private Boolean isSuitableTemp(Double temp) {
+    return temp >= 5 && temp <= 35;
   }
 }
