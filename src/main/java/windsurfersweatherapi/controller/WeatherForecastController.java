d64591e63e6a8613dc183fc6e60e67941abe8676
s.hapughin <s.hapughin@sportradar.com>
2023-11-08 22:57:36 +0100

    feat: add controller ✨

--- src/main/java/windsurfersweatherapi/controller/WeatherForecastController.java
+++ src/main/java/windsurfersweatherapi/controller/WeatherForecastController.java
@@ -1,32 +1,52 @@
 package windsurfersweatherapi.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import windsurfersweatherapi.dto.ForecastDto;
+import windsurfersweatherapi.controller.validator.WeatherForecastControllerValidator;
+import windsurfersweatherapi.dto.WeatherForecastDto;
+import windsurfersweatherapi.mapper.WeatherForecastDtoMapper;
+import windsurfersweatherapi.model.WeatherForecast;
 import windsurfersweatherapi.service.WeatherForecastService;
 
 /**
  * Controller for windsurfers weather forecast.
  */
 @RestController
+@RequestMapping("weatherForecast")
 public class WeatherForecastController {
 
   private final WeatherForecastService weatherForecastService;
-  private final ObjectMapper objectMapper;
+  private final WeatherForecastControllerValidator validator;
+  private final WeatherForecastDtoMapper mapper;
 
+  /**
+   * Constructor.
+   */
   @Autowired
   public WeatherForecastController(
       WeatherForecastService weatherForecastService,
-      ObjectMapper objectMapper
-  ) {
+      WeatherForecastControllerValidator validator,
+      WeatherForecastDtoMapper mapper) {
     this.weatherForecastService = weatherForecastService;
-    this.objectMapper = objectMapper;
+    this.validator = validator;
+    this.mapper = mapper;
   }
 
-  @GetMapping(path = "/")
-  public ForecastDto getBestWindsurfersWeatherForecast() {
-    return new ForecastDto();
+  /**
+   * Path for getting the best location for chosen date.
+   *
+   * @param date requested date
+   * @return WeatherForecastDto
+   */
+  @GetMapping(path = "/bestForWindsurfing")
+  public WeatherForecastDto getBestWindsurfersWeatherForecast(@RequestParam Optional<String> date) {
+    LocalDate localDate = validator.validateAndParseDate(date);
+    WeatherForecast weatherForecast = weatherForecastService.getBestWindsurfersForecast(localDate);
+    return mapper.convertToWeatherForecastsDto(weatherForecast);
   }
 }
